8dae6227d1a8c27dede1e7aae4ce6de85755ad03
ruilongchen27 <ruilongchen27@gmail.com>
2026-03-30 14:25:14 +0900

    更新作业

---
+++ lib/5.dart
@@ -0,0 +1,12 @@
+void main() {
+  int num1 = 10;
+  double num2 = 3.141592;
+
+  String str1 = num1.toString();
+
+  String str2 = num2.toStringAsFixed(2);
+
+
+  print("num1 to str is $str1");
+  print("num2 to str is $str2");
+}
---
+++ lib/6.dart
@@ -0,0 +1,14 @@
+void main() {
+  String s1 = "-12";
+  int num1 = int.parse(s1);
+
+  String s2 = "123.4567";
+  double num2 = double.parse(s2);
+
+  String s3 = "-34.56";
+  num num3 = num.parse(s3);
+
+  print(num1);
+  print(num2);
+  print(num3);
+}
